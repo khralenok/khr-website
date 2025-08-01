026a3abe03fee0af75eca3eff055c2309211bd30
Grigorii Khralenok <grigoriikhralenok@Grigoriis-Mac-mini.local>
2025-08-01 11:49:13 +0700

    Added date formatter to display publication date based on local timezone. Added some minor tweaks here and there on the frontend.

---
+++ app/static/scripts/dates-formatter.js
@@ -0,0 +1,35 @@
+'use srict'
+
+function parseDateString(string) {
+  const [day, mon, year, hm] = string.split(' ');
+  const [hour, min] = hm.split(':');
+  const months = { Jan:0, Feb:1, Mar:2, Apr:3, May:4, Jun:5, Jul:6, Aug:7, Sep:8, Oct:9, Nov:10, Dec:11 };
+
+  return new Date(Date.UTC(year, months[mon], day, hour, min));
+}
+
+const formatTheDate = function(date, options){
+        const rawDate = parseDateString(date.innerHTML)
+        const formatedDate = new Intl.DateTimeFormat('en-GB', options).format(rawDate);
+        date.innerHTML = " " + formatedDate
+}
+
+const registerDates = function(){
+    const dates = document.querySelectorAll('.date');
+    const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+    const options = {
+        day: "2-digit",
+        month: "short",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+        timeZone: userTimeZone, 
+    }
+
+    dates.forEach((date) => formatTheDate(date, options))
+}
+
+
+document.addEventListener('DOMContentLoaded', registerDates)
